54cb8ee9c42307433b40ade73f6eaeab2f3a1892
DEEPAK GADWAL <deepak.gadwal008@@gmail.com>
2024-07-24 13:02:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <div>Count: {count}</div>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
